7b85d578433e6fd76a60e7a853585eca992b568d
Simon Mittag <simonmittag@gmail.com>
2016-04-02 18:29:43 +0700

    added test for Base64

--- src/test/java/com/simonmittag/cryptoutils/symmetric/ByteMeTest.java
+++ src/test/java/com/simonmittag/cryptoutils/symmetric/ByteMeTest.java
@@ -6,6 +6,7 @@ package com.simonmittag.cryptoutils.symmetric;
 import junit.framework.TestCase;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Base64;
 
 /**
  * @author simonmittag
@@ -14,6 +15,7 @@ import java.io.UnsupportedEncodingException;
 public class ByteMeTest extends TestCase {
 
     public void testByteMeIsAlways16Bytes() throws UnsupportedEncodingException {
+        assertTrue(new SymmetricKeyAESCipher().byteMe(Base64.getEncoder().encodeToString("1111222233334444".getBytes())).length==16);
         assertTrue(new SymmetricKeyAESCipher().byteMe("1111222233334444").length==16);
         assertTrue(new SymmetricKeyAESCipher().byteMe("1111222233334444555555555").length==16);
         assertTrue(new SymmetricKeyAESCipher().byteMe("111122223333").length==16);

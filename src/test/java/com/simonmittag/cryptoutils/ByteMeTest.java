bd9e943d6483281f381f7153d4699a74a7f26807
Simon Mittag <simonmittag@gmail.com>
2016-04-03 18:24:06 +0700

    test update

--- src/test/java/com/simonmittag/cryptoutils/ByteMeTest.java
+++ src/test/java/com/simonmittag/cryptoutils/ByteMeTest.java
@@ -3,7 +3,7 @@
  */
 package com.simonmittag.cryptoutils;
 
-import com.simonmittag.cryptoutils.ByteHelper;
+import static com.simonmittag.cryptoutils.ByteHelper.*;
 import junit.framework.TestCase;
 
 import java.io.UnsupportedEncodingException;
@@ -16,11 +16,11 @@ import java.util.Base64;
 public class ByteMeTest extends TestCase {
 
     public void testByteMeIsAlways16Bytes() throws UnsupportedEncodingException {
-        assertTrue(ByteHelper.byteMe(Base64.getEncoder().encodeToString("1111222233334444".getBytes())).length==16);
-        assertTrue(ByteHelper.byteMe("1111222233334444").length==16);
-        assertTrue(ByteHelper.byteMe("1111222233334444555555555").length==16);
-        assertTrue(ByteHelper.byteMe("111122223333").length==16);
-        assertTrue(ByteHelper.byteMe("1").length==16);
-        assertTrue(ByteHelper.byteMe("").length==16);
+        assertTrue(byteMe(Base64.getEncoder().encodeToString("1111222233334444".getBytes())).length==16);
+        assertTrue(byteMe("1111222233334444").length==16);
+        assertTrue(byteMe("1111222233334444555555555").length==16);
+        assertTrue(byteMe("111122223333").length==16);
+        assertTrue(byteMe("1").length==16);
+        assertTrue(byteMe("").length==16);
     }
 }

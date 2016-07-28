1644f8d9f59aafc4cbaa3bfc40a735bd01d137da
Simon Mittag <simonmittag@gmail.com>
2016-07-28 20:40:00 +0800

    added performance test for RSA encryption

--- src/test/java/com/simonmittag/cryptoutils/asymmetric/AsymmetricKeyRSACipherTest.java
+++ src/test/java/com/simonmittag/cryptoutils/asymmetric/AsymmetricKeyRSACipherTest.java
@@ -4,23 +4,61 @@
 package com.simonmittag.cryptoutils.asymmetric;
 
 import com.simonmittag.cryptoutils.SimpleCipher;
+import com.sun.xml.internal.fastinfoset.sax.SystemIdResolver;
 import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import sun.nio.cs.StandardCharsets;
 
+import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.security.SecureRandom;
+import java.util.Random;
+
+import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.Before;
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author simonmittag
  * @since <version>
  */
-public class AsymmetricKeyRSACipherTest extends TestCase {
+public class AsymmetricKeyRSACipherTest {
+    protected String publicKey;
+    protected String privateKey;
+    protected SimpleCipher cipher;
+
+    @Before
+    public void setup() {
+        publicKey = readClassPathResourceAsString("public_key");
+        privateKey = readClassPathResourceAsString("private_key");
+        cipher = new AsymmetricKeyRSACipher(publicKey, privateKey);
+    }
+
+    @Test
     public void testEncryptDecrypt() {
-        String publicKey = readClassPathResourceAsString("public_key");
-        String privateKey = readClassPathResourceAsString("private_key");
-        SimpleCipher cipher = new AsymmetricKeyRSACipher(publicKey, privateKey);
         String encrypted = cipher.encrypt("Hello World");
         String decrypted = cipher.decrypt(encrypted);
         assertEquals(decrypted, "Hello World");
+    }
+
+    @Test
+    public void testEncryptionPerformance() throws UnsupportedEncodingException {
+        int base=1;
+        do {
+            printEncryptionPerformance(base*=2);
+        } while (base<128);
+    }
+
+    public void printEncryptionPerformance(int lengthInBytes) throws UnsupportedEncodingException {
+        String message = "";
+        for(int i=0;i<lengthInBytes;i++) { message += "a"; }
+        long before = System.nanoTime();
+        String encrypted = cipher.encrypt(message);
+        long after = System.nanoTime();
+        long elapsedNanos = after-before;
+        System.out.printf("\nthe encryption of %s bytes took %s nanoseconds (%s microseconds, %s milliseconds)",
+                message.length(), elapsedNanos, elapsedNanos/1000, elapsedNanos/1000000);
     }
 
     protected String readClassPathResourceAsString(String relativePath) {
